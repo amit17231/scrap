3ff2643f952320bdce0eb9f47287edce5aff72cf
Amit Kumar <amitkjcsoftwaresolution@gmail.com>
2023-05-31 21:50:15 +0530

    Commented code

--- api/controllers/ScrapController.js
+++ api/controllers/ScrapController.js
@@ -9,35 +9,35 @@
 //  const cheerio = require('cheerio');
 module.exports = {
   
-    scrapPropertyGruru: async (req, res)=>{
+    // scrapPropertyGruru: async (req, res)=>{
     
             
         
-              try {
-                const url = 'https://www.99.co/singapore/sale?';
-                const response = await axios.get(url);
-                console.log(response.data)
-                const $ = cheerio.load(response.data);
+    //           try {
+    //             const url = 'https://www.99.co/singapore/sale?';
+    //             const response = await axios.get(url);
+    //             console.log(response.data)
+    //             const $ = cheerio.load(response.data);
             
-                // Find and process each property listing
-                const propertyTitles = [];
+    //             // Find and process each property listing
+    //             const propertyTitles = [];
 
-                // $('.listing-card').each((index, element) => {
-                //   const title = $(element).find('.listing-card__title').text().trim();
-                //   propertyTitles.push(title);
-                // });
+    //             // $('.listing-card').each((index, element) => {
+    //             //   const title = $(element).find('.listing-card__title').text().trim();
+    //             //   propertyTitles.push(title);
+    //             // });
 
-                for await (const element of $('.listingCard__titleLink')){
-                  const title = $(element).text().trim();
-                    propertyTitles.push(title);
-                }
+    //             for await (const element of $('.listingCard__titleLink')){
+    //               const title = $(element).text().trim();
+    //                 propertyTitles.push(title);
+    //             }
                 
-                console.log(propertyTitles);
-                res.send(response.data)
-              } catch (error) {
-                console.error('Error:', error.message);
-              }
+    //             console.log(propertyTitles);
+    //             res.send(response.data)
+    //           } catch (error) {
+    //             console.error('Error:', error.message);
+    //           }
        
-    }
+    // }
 };
 
