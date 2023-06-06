935b4cdc21373e3840c190bd9950b8c6b1000ac7
Amit Kumar <amitkjcsoftwaresolution@gmail.com>
2023-06-07 00:10:46 +0530

    Changes

--- config/local.js
+++ config/local.js
@@ -13,4 +13,8 @@ module.exports = {
   // Any configuration settings may be overridden below, whether it's built-in Sails
   // options or custom configuration specifically for your app (e.g. Stripe, Sendgrid, etc.)
 
+  BACK_WEB_URL:"https://scrap-1tnb.onrender.com/",
+  FRONT_WEB_URL:"https://property-frontend-seven.vercel.app/"
+
+
 };
