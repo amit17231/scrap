4c23661f35b4682edc6d824f1a74129d46deaf84
Amit Kumar <dev.amit9352@gmail.com>
2024-09-01 20:24:51 +0530

    updated api url

--- config/local.js
+++ config/local.js
@@ -13,7 +13,7 @@ module.exports = {
   // Any configuration settings may be overridden below, whether it's built-in Sails
   // options or custom configuration specifically for your app (e.g. Stripe, Sendgrid, etc.)
 
-  BACK_WEB_URL:"https://scrap-1tnb.onrender.com/",
+  BACK_WEB_URL:"https://scrap-bj6k.onrender.com/",
   FRONT_WEB_URL:"https://property-frontend-seven.vercel.app/"
 
 
